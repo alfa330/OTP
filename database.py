3f2ba9a65366099afc2c98d4b9bc706df24f34a5
alfa330 <rightleft673@gmail.com>
2025-10-22 11:59:30 +0500

    Обновляю имя у 133

--- database.py
+++ database.py
@@ -81,8 +81,8 @@ class Database:
             # Users table (без direction_id на этом этапе)
             cursor.execute("""
                 UPDATE users
-                SET name = 'Чингиз Бактыгул'
-                WHERE id = 120;
+                SET name = 'Алибек Аружан'
+                WHERE id = 133;
                 CREATE TABLE IF NOT EXISTS users (
                     id SERIAL PRIMARY KEY,
                     telegram_id BIGINT UNIQUE,
