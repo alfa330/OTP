aec9bf45e6bf62218542124c2c444e938b86fcb2
Ruslan <rightleft673@gmail.com>
2026-03-06 01:33:36 +0500

    Исправление моментов

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './call_evaluation.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
